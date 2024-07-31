df3372b666ff96d67894a303bd1ca3ffe1487651
AKDavis95 <112223134+AKDavis95@users.noreply.github.com>
2024-07-31 17:54:57 -0400

    adding scroll icon

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from 'next/image';
 import Navbar from './components/Navbar';
 import './scss/style.scss';
 import Section1 from './components/Section1';
+import Scroll from './components/Scroll';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Section1 />
+      <Scroll />
     </>
   );
 }
